3d6bdb6f5764abc3dac408bccce63278deafa362
Mikhail Kozlov <kozlov_m_v@mail.ru>
2023-02-18 19:43:21 +0300

    add task 1

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+int enterNumber (string msg)
+{
+    System.Console.Write($"{msg} -> ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = enterNumber("Введите 3-х значное число");
+
+if (number > 100 && number < 1000)
+{
+    int secondDigit = (number / 10) % 10;
+    System.Console.WriteLine($"Вторая цифра числа {number} равна {secondDigit}");
+}
+else
+{
+    System.Console.WriteLine($"Число {number} не 3-х значное");
+}
